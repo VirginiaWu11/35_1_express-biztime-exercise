3a517df129caeeaf933e764312631c0a0b620238
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-25 20:22:48 -0400

    add get companies route

--- routes/companies.js
+++ routes/companies.js
@@ -4,3 +4,12 @@ const router = express.Router();
 const db = require("../db");
 
 module.exports = router;
+
+router.get("/", async (req, res, next) => {
+    try {
+        const result = await db.query(`SELECT * FROM companies`);
+        return res.json({ companies: result.rows });
+    } catch (e) {
+        next(e);
+    }
+});
