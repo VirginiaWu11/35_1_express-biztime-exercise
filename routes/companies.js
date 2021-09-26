c4e2602da319d45b7b3612a7ee82e9928519d10e
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-25 20:45:20 -0400

    add GET single company and POST new company route

--- routes/companies.js
+++ routes/companies.js
@@ -7,8 +7,40 @@ module.exports = router;
 
 router.get("/", async (req, res, next) => {
     try {
-        const result = await db.query(`SELECT * FROM companies`);
-        return res.json({ companies: result.rows });
+        const results = await db.query(`SELECT * FROM companies`);
+        return res.json({ companies: results.rows });
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.get("/:code", async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const results = await db.query(
+            `SELECT * FROM companies WHERE code = $1`,
+            [code]
+        );
+        if (results.rows.length === 0) {
+            throw new ExpressError(
+                `Can't find company with code of ${code}`,
+                404
+            );
+        }
+        return res.json({ companies: results.rows });
+    } catch (e) {
+        next(e);
+    }
+});
+
+router.post("/", async (req, res, next) => {
+    try {
+        const { code, name, description } = req.body;
+        const results = await db.query(
+            `INSERT INTO companies (code, name, description) VALUES ($1,$2,$3) RETURNING code, name, description`,
+            [code, name, description]
+        );
+        return res.status(201).json({ companies: results.rows[0] });
     } catch (e) {
         next(e);
     }
