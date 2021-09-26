f789d21bef234394afa7a9ae67d4b879e11dd075
Virginia Wu <79432798+VirginiaWu11@users.noreply.github.com>
2021-09-25 20:58:54 -0400

    add put / update single company request

--- routes/companies.js
+++ routes/companies.js
@@ -45,3 +45,23 @@ router.post("/", async (req, res, next) => {
         next(e);
     }
 });
+
+router.put("/:code", async (req, res, next) => {
+    try {
+        const { code } = req.params;
+        const { name, description } = req.body;
+        const results = await db.query(
+            `UPDATE companies SET name=$1, description=$2 WHERE code=$3 RETURNING code, name, description`,
+            [name, description, code]
+        );
+        if (results.rows.length === 0) {
+            throw new ExpressError(
+                `Can't find company with code of ${code}`,
+                404
+            );
+        }
+        return res.status(201).json({ companies: results.rows[0] });
+    } catch (e) {
+        next(e);
+    }
+});
